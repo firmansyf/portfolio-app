997cc52e455d70ed12974fe7a792b577effb5ddc
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2022-07-24 21:50:16 +0700

    initialization again

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BestPractice } from './page/best-practice/Practice'
+import { Blog } from './page/blog/Blog'
+import { Home } from './page/home/Home'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
+
+  const customStyle : any = {
+    style : 'text-white m-2'
+  }
+
   return (
-   <></>
+   <>
+    <Router>
+       <>
+
+          <Navbar bg="dark" variant="dark">
+            <Container className=''>
+              <Navbar.Brand>Yusuf Firmansyah</Navbar.Brand>
+              <Nav className="me-auto w-100">
+               <Link className={`nav-link ${customStyle?.style}`} to="/">Home</Link>
+               <Link className={`nav-link ${customStyle.style}`} to="/best-pratice">Best Practice</Link>
+               <Link className={`nav-link ${customStyle.style}`} to="/blog">Blog</Link>
+              </Nav>
+            </Container>
+          </Navbar>
+
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/best-pratice' element={<BestPractice/>} />
+            <Route path='/blog'  element={<Blog/>} />
+          </Routes>   
+        </>
+      </Router>
+   </>
   );
 }
 
-export default App;
+export default App
---
+++ src/page/home/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Home : FC = () => {
+    return(
+        <>
+            <h1>Home Page...</h1>
+        </>
+    )
+}
+
+export { Home }
